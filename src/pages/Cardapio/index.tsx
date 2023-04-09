5cef83673651d03280775ac61217e8393d518745
Kayque Almeida <kayquefsa@gmail.com>
2023-04-09 13:02:36 -0300

    updating the project

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
 import {ReactComponent as Logo} from 'assets/logo.svg';
 
 export default function Cardapio() {
+
+  const [busca, setBusca] = useState("");
+
   return (
     <main>
       <nav className={styles.menu}>
@@ -10,6 +15,13 @@ export default function Cardapio() {
       <header className={styles.header}>
         <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardaio__titulo}>Cardápio</h3>
+        <Buscador 
+          busca={busca} 
+          setBusca={setBusca}
+        />
+      </section>
     </main>
   )
 }
