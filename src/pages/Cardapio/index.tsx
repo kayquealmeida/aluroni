377b3fcf051015e05a1fb4f1716a1339ef1e7820
Kayque Almeida <kayquefsa@gmail.com>
2023-04-09 16:47:25 -0300

    Updating the project

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,10 +2,14 @@ import { useState } from 'react';
 import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
 import {ReactComponent as Logo} from 'assets/logo.svg';
+import Filtros from './Filtros';
+import Ordenador from './Ordenador';
 
 export default function Cardapio() {
 
   const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
 
   return (
     <main>
@@ -21,6 +25,13 @@ export default function Cardapio() {
           busca={busca} 
           setBusca={setBusca}
         />
+        <div className={styles.cardapio__filtros}>
+          <Filtros 
+            filtro={filtro}
+            setFiltro={setFiltro}
+          />
+        </div>
+        <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
       </section>
     </main>
   )
